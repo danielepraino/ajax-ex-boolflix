b17e96dfa17fd8a9fbb299422d8927fb3c2d72b1
Daniele Praino <daniele.praino@gmail.com>
2019-05-27 23:44:24 +0200

    boolflix_v1 milestone 2 work in progress

--- boolflix_v1/script.js
+++ boolflix_v1/script.js
@@ -40,6 +40,24 @@ function callIMBDAPI(titleName){
   });
 }
 
+//creo una funzione che converte il punteggio in un range da 1 a 5 e lo arrotonda
+//in base al punteggio assegno le stelline
+function setScore(titleScore){
+  var prepared = Math.round(titleScore)/2;
+  for (var i = 1; i <= prepared; i++) {
+    $(".star[data-star=" + i + "]").find("i").removeClass("far").addClass("fas yellow");
+  }
+}
+
+//NOTA: problema lingua da iso 639-1 a 639-2 da risolvere
+function setFlag(titleLanguage){
+  if (titleLanguage == "en"){
+    $(".flag").find("span").addClass("flag-icon flag-icon-gb");
+  } else {
+    $(".flag").find("span").addClass("flag-icon flag-icon-" + titleLanguage);
+  }
+}
+
 //al click del bottone faccio una chiamata all'API di IMDB e creo una card che contiene
 //tutti i dati relativi al film cercato. Ho notato che c'è un piccolo ritardo nella chiamata
 //se genero la card instantaneamente sarà vuota, ho dovuto quindi inserire un setTimeout
@@ -47,9 +65,13 @@ function callIMBDAPI(titleName){
 $(".btn").click(function(){
   $(".main").empty();
   var searchedTitle = $(".input-search").val();
-  console.log("searchedTitle: " + searchedTitle);
-  callIMBDAPI(searchedTitle);
-  setTimeout(function(){
-    $(".main").append(movieTemplate(card));
-  }, 500);
+  if (searchedTitle != "") {
+    console.log("searchedTitle: " + searchedTitle);
+    callIMBDAPI(searchedTitle);
+    setTimeout(function(){
+      $(".main").append(movieTemplate(card));
+      setFlag(card.language);
+      setScore(card.vote);
+    }, 500);
+  }
 });
